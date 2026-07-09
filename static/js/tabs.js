69330c8f0a79a45157b0832f7023f95ac974df66
Tom Windels <tom.windels@outlook.com>
2026-07-09 15:20:21 +0200

    Add report overview and layout improvements
    Added a new partial that contains a tabbed container of all detecter reports, listing them as an overview for the corresponding graphs
    Improved the layout of graph items, so they preserve aspect ratio regardless of the scale of the surrounding content

---
+++ static/js/tabs.js
@@ -0,0 +1,19 @@
+// src: https://dongheenam.info/posts/tabs-in-hugo/
+function toggleActive(element, condition) {
+  if (condition) {
+    element.classList.add("active");
+  } else {
+    element.classList.remove("active");
+  }
+};
+function switchTab(groupId, name) {
+  const tabItems = document.querySelectorAll(
+    `.tab-item[data-tab-group=${groupId}]`
+  );
+  const tabButtons = document.querySelectorAll(
+    `.tab-button[data-tab-group=${groupId}]`
+  );
+  [...tabItems, ...tabButtons].forEach(
+    (item) => toggleActive(item, item.dataset.tabItem === name)
+  );
+};
